9de81770ad6a3a89ad50490128188fb130ed9577
bvk2412001 <khoakl241@gmail.com>
2025-08-02 10:05:37 +0700

    no message

---
+++ assets/Script/Manager/LoadingManager.ts
@@ -0,0 +1,66 @@
+import { _decorator, Component, director, Label, Node, Sprite, tween } from 'cc';
+import { FbSdk } from '../FbSdk';
+const { ccclass, property } = _decorator;
+
+@ccclass('LoadingManager')
+export class LoadingManager extends Component {
+    @property(Node)
+    loadingbar: Node = null
+
+    @property(Label)
+    txtLoadingPersent: Label = null
+
+
+    protected onLoad(): void {
+        this.FXLblLoading()
+        this.FXProgressLoading()
+        this.LoadScene()
+
+    }
+
+    protected start(): void {
+        FbSdk.getInstance().loginGame()
+    }
+
+    LoadScene() {
+        director.preloadScene("Gameplay", (completedCount, totalCount, item) => {
+
+        }, () => {
+            this.scheduleOnce(() => {
+                director.loadScene("Gameplay");
+            }, 0.5)
+
+        });
+    }
+
+    FXLblLoading() {
+        const label = this.txtLoadingPersent;
+        const texts = ["Loading.", "Loading..", "Loading..."];
+        let index = 0;
+
+        this.schedule(() => {
+            label.string = texts[index];
+            index = (index + 1) % texts.length;
+        }, 0.5);
+    }
+
+
+    FXProgressLoading() {
+        const loadingSprites: Node[] = this.loadingbar.children
+        const totalSprites = loadingSprites.length;
+        const duration = 10; // 3 giây
+        const interval = 1
+
+        for (let i = 0; i < totalSprites; i++) {
+            this.scheduleOnce(() => {
+                tween(loadingSprites[i].getComponent(Sprite)).to(interval, { fillRange: 1 }).start()
+            }, interval * i)
+        }
+
+
+    }
+
+
+}
+
+
